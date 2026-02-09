8cec720f4ef2451bd99108c22d97d8bc351b7a3d
bimbiriim <is.gainullin@innopolis.university>
2026-02-09 13:39:42 +0300

    Init processWire in /public and add .gitignore

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'processwire/processwire',
+        'pretty_version' => '3.0.255',
+        'version' => '3.0.255.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'processwire/processwire' => array(
+            'pretty_version' => '3.0.255',
+            'version' => '3.0.255.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
